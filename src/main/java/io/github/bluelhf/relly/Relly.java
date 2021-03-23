56ca011445dbce7ddac8452d1d45c88fab736c6d
BuildTools <unconfigured@null.spigotmc.org>
2021-03-23 16:02:46 +0200

    Fix file monitor
    Took 29 seconds

--- src/main/java/io/github/bluelhf/relly/Relly.java
+++ src/main/java/io/github/bluelhf/relly/Relly.java
@@ -5,38 +5,50 @@ import dev.jorel.commandapi.CommandAPI;
 import io.github.bluelhf.relly.command.RellyCommand;
 import io.github.bluelhf.relly.util.FileMonitor;
 import io.github.bluelhf.relly.util.RellyUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public final class Relly extends BukkitPlugin {
 
     public static final List<String> BLACKLIST = Arrays.asList("FastAsyncWorldEdit", "WorldEdit");
 
-    FileMonitor pluginMonitor = new FileMonitor(getDataFolder().getParentFile(), Duration.ofSeconds(5), (file, change) -> {
+    private final HashMap<File, String> fileNameMap = new HashMap<>();
 
+    FileMonitor pluginMonitor = new FileMonitor(getDataFolder().getParentFile(), Duration.ofSeconds(5), (file, change) -> {
+        refreshMap();
         if (!file.getName().endsWith(".jar")) return;
-        if (RellyUtil.isLoaded(file)) {
-            JavaPlugin plugin;
-            if ((plugin = RellyUtil.getJavaPlugin(file)) == null) return;
-            switch (change) {
-                case MODIFY:
-                    RellyUtil.reloadPlugin(plugin, true);
-                    break;
-                case REMOVE:
-                    RellyUtil.disablePlugin(plugin, true);
-                    break;
+        Bukkit.getScheduler().runTask(this, () -> {
+            String name;
+            Plugin plugin;
+            if ((name = fileNameMap.get(file)) != null && (plugin = Bukkit.getPluginManager().getPlugin(name)) != null) {
+                switch (change) {
+                    case ADD:
+                        if (plugin.isEnabled()) RellyUtil.disablePlugin(plugin, false);
+                        RellyUtil.enablePlugin(file.toPath());
+                        break;
+                    case MODIFY:
+                        RellyUtil.reloadPlugin(plugin, true);
+                        break;
+                    case REMOVE:
+                        RellyUtil.disablePlugin(plugin, true);
+                        break;
+                }
+            } else {
+                switch (change) {
+                    case ADD:
+                    case MODIFY:
+                        System.out.println(RellyUtil.enablePlugin(file.toPath()));
+                        break;
+                }
             }
-        } else {
-            switch (change) {
-                case ADD:
-                case MODIFY:
-                    RellyUtil.enablePlugin(file.toPath());
-                    break;
-            }
-        }
+        });
     });
 
 
@@ -49,6 +61,12 @@ public final class Relly extends BukkitPlugin {
     public void startup() {
         pluginMonitor.start();
         CommandAPI.onEnable(this);
+    }
+
+    private void refreshMap() {
+        for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+            fileNameMap.put(RellyUtil.getPluginSource(plugin), plugin.getDescription().getName());
+        }
     }
 
     @Override
